326b64d537749f1d1baf95862c4709ee8d26ffc5
rashfael <rashfael@isobeef.org>
2020-02-02 10:55:00 +0100

    initial stuff

---
+++ src/carlo_main.js
@@ -0,0 +1,27 @@
+const carlo = require('carlo')
+const isDevelopment = process.env.NODE_ENV !== 'production'
+;(async () => {
+	// Launch the browser.
+	const app = await carlo.launch({
+		width: 800,
+		height: 600,
+		args: isDevelopment
+			? ['--auto-open-devtools-for-tabs', '--ignore-certificate-errors']
+			: []
+	})
+
+	// Terminate Node.js process on app window closing.
+	app.on('exit', () => process.exit())
+
+	// Tell carlo where your web files are located.
+	if (isDevelopment) {
+		app.serveOrigin(process.env.WEBPACK_DEV_SERVER_URL)
+	} else {
+		app.serveFolder(__dirname)
+	}
+
+	await app.exposeFunction('env', () => process.env)
+
+	// Navigate to the main page of your app.
+	await app.load('index.html')
+})()
---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const webpack = require('webpack')
+// const targetStub = process.env.NODE_ENV === 'development' ? 'dev' : 'prod'
+
+module.exports = {
+	devServer: {
+		host: '0.0.0.0',
+		port: 8880
+	},
+	transpileDependencies: ['buntpapier'],
+	configureWebpack: {
+		resolve: {
+			symlinks: false, // don't flatten symlinks (for npm link)
+			modules: [path.resolve('src'), path.resolve('src/styles'), 'node_modules'],
+			// alias: {
+			// 	'config': path.resolve(__dirname, `config.${targetStub}.js`)
+			// }
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'DEVELOPMENT': process.env.NODE_ENV === 'development'
+			})
+		],
+	},
+	css: {
+		loaderOptions: {
+			stylus: {
+				use: [require('buntpapier/stylus')()]
+			}
+		}
+	},
+	lintOnSave: true
+}
